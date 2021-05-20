b0365f0237178f91402a3709b42a9050cd454ed6
cperez <chrper31@gmail.com>
2021-05-20 15:23:06 +0200

    initial commit : cratioon 1er activity

---
+++ app/src/main/java/com/perez/christophe/topquiz/controller/MainActivity.java
@@ -0,0 +1,65 @@
+package com.perez.christophe.topquiz.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.perez.christophe.topquiz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+
+        mPlayButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // active the button when writed text
+                mPlayButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // user clicked the button for change first view for second view
+
+
+
+            }
+        });
+
+    }
+}

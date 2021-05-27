e5780d5eeec3ebbea25433e7a0b42a262bde70fc
cperez <chrper31@gmail.com>
2021-05-27 17:56:21 +0200

    Cycle de vie d'une activité ajout des methodes system pour laisser une "trace"  qui sera visible dans le Logcat en faisant appel à System.out.prinln

--- app/src/main/java/com/perez/christophe/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/perez/christophe/topquiz/controller/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("MainActivity::onCreate()");
+
         mUser = new User(); // 2/3 : pour initialiser la variable mUser
 
         mPreferences = getPreferences(MODE_PRIVATE); // 2/4 : pour initialiser la variable mPreferences
@@ -116,5 +118,40 @@ public class MainActivity extends AppCompatActivity {
             mNameInput.setSelection(firstname.length());
             mPlayButton.setEnabled(true);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        System.out.println("MainActivity::onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        System.out.println("MainActivity::onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        System.out.println("MainActivity::onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        System.out.println("MainActivity::onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        System.out.println("MainActivity::onDestroy()");
     }
 }
